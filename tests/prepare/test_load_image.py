ee618bfc73854f3e430244f43feb03a9df6323c0
Tenzin <tenzintsunduebhattu@gmail.com>
2024-04-19 16:43:44 +0530

    add more test cases for load image

--- tests/prepare/test_load_image.py
+++ tests/prepare/test_load_image.py
@@ -5,10 +5,25 @@ from monocheck.prepare import load_image
 
 def test_load_image():
     DATA_DIR = Path(__file__).parent / "data"
-    image_path = DATA_DIR / "image.jpg"
+    
+    image_path = DATA_DIR / "pecha_I1KG812750008.jpg"
     img_array = load_image(image_path)
     assert isinstance(img_array, np.ndarray)
     assert img_array.shape == (1, 224, 224, 3)
 
+    image_path = DATA_DIR / "cropped_empty.jpg"
+    img_array = load_image(image_path)
+    assert isinstance(img_array, np.ndarray)
+    assert img_array.shape == (1, 224, 224, 3)
+
+    image_path = DATA_DIR / "cropped_empty_small.jpg"
+    img_array = load_image(image_path)
+    assert isinstance(img_array, np.ndarray)
+    assert img_array.shape == (1, 224, 224, 3)
+
+    image_path = DATA_DIR / "pecha_line_image.jpg"
+    img_array = load_image(image_path)
+    assert isinstance(img_array, np.ndarray)
+    assert img_array.shape == (1, 224, 224, 3)
 
 test_load_image()
