b574719d93ca6efe8b35113e75f04d4b3276652d
Tenzin <tenzintsunduebhattu@gmail.com>
2024-04-22 11:32:05 +0530

    dimension reduction using pca

--- src/monocheck/pipeline.py
+++ src/monocheck/pipeline.py
@@ -7,7 +7,7 @@ from keras.models import Model
 
 from monocheck.prepare import load_image
 from monocheck.feature_extraction import extract_features
-
+from monocheck.dimension_reduction import reduce_dimension
 
 def pipeline(image_paths:List[Path]):
     imgs_array = [load_image(image_path).squeeze(0) for image_path in image_paths]
@@ -16,11 +16,14 @@ def pipeline(image_paths:List[Path]):
     model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
 
     imgs_features = extract_features(imgs_array, model)
-    return imgs_features
-
+    imgs_features = imgs_features.reshape(-1,4096)
+    reduced_imgs_features = reduce_dimension(imgs_features)
+    return reduced_imgs_features
 
 if __name__ == "__main__":
     imgs_path = [Path("image.jpg"), Path("image2.jpg")]
+    imgs_feat = pipeline(imgs_path)
+    print(imgs_feat)
 
     
 
